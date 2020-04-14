d1a9913cb2b0a3097611a1ac6d0599857e1bec38
Jyoti2808 <63653029+Jyoti2808@users.noreply.github.com>
2020-04-14 23:14:49 +0530

    Add files via upload

--- delete_files.sh
+++ delete_files.sh
@@ -4,8 +4,8 @@
 
 read -p "Enter the file size with unit c or k or M" size 
 
-find /home/adii/Desktop  -size -$size  -print
+find /home/jyoti/Desktop  -size -$size  -print
 
-find /home/adii/Desktop  -size -$size -delete
+find /home/jyoti/Desktop  -size -$size -delete
    
 echo "FILES DELETED BELOW GIVEN SIZE"
